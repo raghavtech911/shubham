61d8d2bfe1dd3ac291fffa52fc1152074707de62
raghavtech911 <raghavendra.solanki@techinfini.com>
2017-09-30 16:53:00 +0530

    30sept17

--- postdragdrop/another/script.js
+++ postdragdrop/another/script.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-
-
-$('.box-item').draggable({
-    cursor: 'move',
-    helper: "clone"
-  });
-
-
+ 
 /* on hover slidedown */
 $('.flip1').mouseover(function(){
         $('.panel1').slideDown('slow');
@@ -30,7 +23,6 @@ $('.flip5').mouseover(function(){
 $('.flip6').mouseover(function(){
         $('.panel6').slideDown('slow');
     });
-
 
 
 /* on click slideup */
@@ -66,75 +58,83 @@ $('.flip6').click(function(){
 //     });
 
 
-  var string1 = {};
-  var category1 = [] 
-   var string2 = {};
-  var category2 = [] 
-   var string3 = {};
-  var category3 = [] 
-   var string4 = {};
-  var category4 = [] 
-   var string5 = {};
-  var category5 = [] 
-   var string6 = {};
-  var category6 = []
+  // var string1 = {};
+  // var category1 = [] 
+  //  var string2 = {};
+  // var category2 = [] 
+  //  var string3 = {};
+  // var category3 = [] 
+  //  var string4 = {};
+  // var category4 = [] 
+  //  var string5 = {};
+  // var category5 = [] 
+  //  var string6 = {};
+  // var category6 = []
 
+var arr0=[]; var arr1=[]; var arr2=[]; var arr3=[]; var arr4=[]; var arr5=[]; var arr6=[]; var arr7=[]; var arr8=[]; var arr9=[]; 
+var arr10=[]; var arr11=[]; var arr12=[]; 
+
+
+$('.box-item').draggable({
+    cursor: 'move',
+    helper: "clone",
+    revert: 'invalid'
+  });
 
 
   $("#container1").droppable({
     drop: function(event, ui) {
       var itemid = $(event.originalEvent.toElement).attr("itemid");
-      //$('.newaddedclass').each(function() {
-       // if ($(this).attr("itemid") === itemid) {
-          //$(this).appendTo("#container1");
-        }
-      //});
-    //}
+            ui.draggable.remove(); // to remove the dragged item
+            var k =ui.draggable.attr("itemid");
+            //if($(".box-item").attr("itemid")== k ){
+            $(".box-item").removeClass("green"); // to remove the green background
+           //}
+           //remove the element form array 
+            arr2.pop(itemid);
+            arr4.pop(itemid);
+            arr6.pop(itemid);
+            arr8.pop(itemid);
+            arr10.pop(itemid);
+            arr12.pop(itemid);
+            console.log(arr0);
+    }
   });
 
-var arr1=[]; var arr2=[]; var arr3=[]; var arr4=[]; var arr5=[]; var arr6=[]; var arr7=[]; var arr8=[]; var arr9=[]; var arr10=[];
-var arr11=[]; var arr12=[]; var arr0=[];
 
   $("#category1").droppable({
     drop: function(event, ui) {
-
-        $(ui.draggable).addClass("newaddedclass");
-        $(ui.draggable).addClass("green");
-       
-       // $(ui.draggable).attr('id', 'newid');
-       // console.log( event.target.id );
-        
-        //         arr1[i].pop(event.target.id);  
-        //arr1.push(event.target.id);
         
         var itemid = $(event.originalEvent.toElement).attr("itemid");
-        
-       // $('.newaddedclass').each(function() {
-           // if ($(this).attr("itemid") === itemid) {
-                   // var draggable = ui.draggable;
-                   // var id = draggable.attr("itemid");
-                    
+       // alert(itemid);
+        var itemid1 = $(ui.draggable).attr("itemid");
+       // alert(itemid1); 
+        //alert($("#"+itemid1).attr("id"));
+        // if($(this).children(".box-item").attr("id") == itemid1) {
+        // alert("cancel");    //Instead want to cancel dragging of item
+        //     return false;
+        //   //  location.reload();
+        //  }else{            
+
+            if($(ui.draggable).attr("itemid") === $("#category1 > #"+itemid1).attr("id")){
+                alert('Can Drag One Post Only Once In One Category');
+            }
+                else{
+                   // alert('in else');
                     tag=ui.draggable;
-                    tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category1" );
+                    var a = tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category1" ).draggable({ cursor: 'move', 
+                            helper: "clone", revert: 'invalid'});
+
+                    $(a).addClass("newaddedclass");
+                    $(a).removeClass("green");
+                    $(ui.draggable).addClass("green");
                     
-                    // var dell = "<span style='text-align: right;' id='dell'>delete</span>";
-                    // $(".newaddedclass").after(dell);
-                    // $("#dell").click(function(){
-                    //     $(".green").remove();
-                    // });
- 
-               // $(this).clone().appendTo("#category1");
-                
+                    $(a).attr('id',itemid);
+
                 
                     // var employee = {
                     // "key": itemid,
                     // }
-                    
-                 //    for(i=0; i<=20; i++){
-                 // var employee = {
-                 //     "status":[[i],[itemid]]
-                 //        }            
-                 //     }  
 
                 //string1.category1.push(employee);
                //  console.log(JSON.stringify(string1));
@@ -142,45 +142,63 @@ var arr11=[]; var arr12=[]; var arr0=[];
            
                 var type='category1';
                 arr2.push(itemid);
-                arr1[type]= arr2;
-                console.log(arr1);
-
-            //}
-        //});
-    }//,
-    // accept: function(draggable) {
-     //$(this).find("#copy-" + draggable.attr("itemid")).length == 0;
-  //}
+                arr1[type]= arr2; 
+            }
+    }
    });
+  arr0.push(arr1);
+//  console.log(arr0);
+
 
   $("#category2").droppable({
         drop: function(event, ui) {
-            //$(ui.draggable).addClass("newaddedclass");
-            $(ui.draggable).addClass("green");
             var itemid = $(event.originalEvent.toElement).attr("itemid");
+            var itemid1 = $(ui.draggable).attr("itemid");
+            if($(ui.draggable).attr("itemid") === $("#category2 > #"+itemid1).attr("id")){
+                alert('Can Drag One Post Only Once In One Category');
+            }
+                else{
             tag=ui.draggable;
-            tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category2" );
+            var a = tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category2" ).draggable({cursor: 'move',
+                    helper: "clone", revert: 'invalid'});
+            $(a).addClass("newaddedclass");
+            $(a).removeClass("green");
+            $(ui.draggable).addClass("green");
+            $(a).attr('id',itemid);
             var type='category2';
             arr4.push(itemid);
             arr3[type]= arr4;
-            console.log(arr3);
+            // console.log(arr3);
         }
+    }
 });
+   arr0.push(arr3);
+//  console.log(arr0);
 
 
   $("#category3").droppable({
     drop: function(event, ui) {
-        $(ui.draggable).addClass("newaddedclass");
-          $(ui.draggable).addClass("green");
         //console.log( event.target.id );
         
         // string3.category3 = category3;
       var itemid = $(event.originalEvent.toElement).attr("itemid");
+       var itemid1 = $(ui.draggable).attr("itemid");
+        if($(ui.draggable).attr("itemid") === $("#category3 > #"+itemid1).attr("id")){
+                alert('Can Drag One Post Only Once In One Category');
+            }
+                else{
+             //       if($("#category3 > #"+itemid1).attr("id")==$("#category3 > #"+itemid1).attr("id")){
+               //         alert('same element');
+                 //   }
       //$('.newaddedclass').each(function() {
        // if ($(this).attr("itemid") === itemid) {
         //  $(this).clone().appendTo("#category3");
         tag=ui.draggable;
-                    tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category3" );
+                    var a = tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category3" ).draggable({ cursor: 'move',
+                            helper: "clone", revert: 'invalid' });
+            $(a).addClass("newaddedclass");
+            $(ui.draggable).addClass("green");
+            $(a).attr('id',itemid);
             //         var employee = {
             //         key: itemid,              
             // }
@@ -190,28 +208,38 @@ var arr11=[]; var arr12=[]; var arr0=[];
                 var type='category3';
                 arr6.push(itemid);
                 arr5[type]= arr6;
-                console.log(arr5);
+                //console.log(arr5);
                }
+           }
         //   });
         // }
   });
+  arr0.push(arr5);
 
 
 
     $("#category4").droppable({
         drop: function(event, ui) {
-            $(ui.draggable).addClass("newaddedclass");
-              $(ui.draggable).addClass("green");
+            
             //console.log( event.target.id );
             
             //string4.category4 = category4;
             var itemid = $(event.originalEvent.toElement).attr("itemid");
+            var itemid1 = $(ui.draggable).attr("itemid");
+        if($(ui.draggable).attr("itemid") === $("#category4 > #"+itemid1).attr("id")){
+                alert('Can Drag One Post Only Once In One Category');
+            }
+                else{
            // $('.newaddedclass').each(function() {
              //   if ($(this).attr("itemid") === itemid) {
                //     $(this).clone().appendTo("#category4");
 
-               tag=ui.draggable;
-                    tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category4" );
+                    tag=ui.draggable;
+                    var a = tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category4" ).draggable({ cursor: 'move', 
+                            helper: "clone", revert: 'invalid' });
+                    $(a).addClass("newaddedclass");
+                    $(ui.draggable).addClass("green");
+                        $(a).attr('id',itemid);
             //         var employee = {
             //         key: itemid,
             // }
@@ -219,81 +247,63 @@ var arr11=[]; var arr12=[]; var arr0=[];
             //         console.log(JSON.stringify(string4));
 
                     var type='category4';
-                arr8.push(itemid);
-                arr7[type]= arr8;
-                console.log(arr7);
+                    arr8.push(itemid);
+                    arr7[type]= arr8;
         }
+    }
     //   });
     // }
   });
-
-
+    arr0.push(arr7);
 
   $("#category5").droppable({
     drop: function(event, ui) {
-        $(ui.draggable).addClass("newaddedclass");
-          $(ui.draggable).addClass("green");
-        //console.log( event.target.id );
-         
-         // string5.category5 = category5;
+        
       var itemid = $(event.originalEvent.toElement).attr("itemid");
-      // $('.newaddedclass').each(function() {
-      //   if ($(this).attr("itemid") === itemid) {
-      //     $(this).clone().appendTo("#category5");
+      var itemid1 = $(ui.draggable).attr("itemid");
+        if($(ui.draggable).attr("itemid") === $("#category5 > #"+itemid1).attr("id")){
+                alert('Can Drag One Post Only Once In One Category');
+            }
+                else{
       tag=ui.draggable;
-                    tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category5" );
-            //         var employee = {
-            //         key: itemid,   
-            // }
-            //     string5.category5.push(employee);
-            //         console.log(JSON.stringify(string5));
-
+                    var a = tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category5" ).draggable({ cursor: 'move',
+                            helper: "clone", revert: 'invalid' });
+                    $(a).addClass("newaddedclass");
+                     $(ui.draggable).addClass("green");
+                         $(a).attr('id',itemid);
 
                 var type='category5';
                 arr10.push(itemid);
                 arr9[type]= arr10;
-                console.log(arr9);
         }
-    //   });
-    // }
+    }
   });
-
+  arr0.push(arr9);
 
 
   $("#category6").droppable({
     drop: function(event, ui) {
-        $(ui.draggable).addClass("newaddedclass");
-          $(ui.draggable).addClass("green");
-       // console.log( event.target.id );
-        
-        // string6.category6 = category6;
-      var itemid = $(event.originalEvent.toElement).attr("itemid");
-      // $('.newaddedclass').each(function() {
-      //   if ($(this).attr("itemid") === itemid) {
-      //     $(this).clone().appendTo("#category6");
-      tag=ui.draggable;
-                    tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category6" );
-            //         var employee = {
-            //         i: itemid,
-            // }
-            //     string6.category6.push(employee);
-            //     console.log(JSON.stringify(string6));
+        var itemid = $(event.originalEvent.toElement).attr("itemid");
+        var itemid1 = $(ui.draggable).attr("itemid");
+        if($(ui.draggable).attr("itemid") === $("#category6 > #"+itemid1).attr("id")){
+                alert('Can Drag One Post Only Once In One Category');
+            }
+                else{
+        tag=ui.draggable;
+        var a = tag.clone().attr("itemid", "copy-" + tag.attr("itemid")).appendTo("#category6" ).draggable({ cursor: 'move',
+                helper: "clone", revert: 'invalid' });
+        $(a).addClass("newaddedclass");
+        $(ui.draggable).addClass("green");
+            $(a).attr('id',itemid);
 
                 var type='category6';
                 arr12.push(itemid);
                 arr11[type]= arr12;
-                console.log(arr11);
         }
-    //   });
-    // }
+    }
   });
-
-
-  // $(".green").draggable({
-  //       drop: function(event, ui) {
-  //         //  $(ui.dragable).draggable('destroy');
-  //            // $(ui.dragable).remove();
-  //       }
-  // });
+  arr0.push(arr11);
+  console.log(arr0);
+  //console.log(arr0.toString());
 
 });
